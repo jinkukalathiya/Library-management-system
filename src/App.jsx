c95b328f27dc7b59ddca78951d71de7bbb0dd3ac
vivek kachhadiya <ifmdac80@gmail.com>
2025-08-02 15:03:45 +0530

    02-08-2025

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Register from './pages/Register'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/'></Route>
           <Route path='/login' />
+          <Route path = '/register' element={<Register/>}/>
         </Routes>
       </BrowserRouter>
-      <h1>Helloooooo</h1>
     </>
   )
 }
